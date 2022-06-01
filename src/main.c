9ffa04bf5d26048746b0acb63d6062c46c0b2ddf
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2022-06-01 09:25:10 +0200

    simplify error handling

--- src/main.c
+++ src/main.c
@@ -4,12 +4,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <errno.h>
 #include <signal.h>
 #include <spawn.h>
+#include <string.h>
 #include <time.h>
 #include <unistd.h>
 
 #include <pthread.h>
+
+#define TRY(x) \
+    do { \
+        int result; \
+        if ((result = (x)) != 0) { \
+            result = result == -1 ? errno : result; \
+            fprintf(stderr, "ERROR: %s (%s L%d)\n", strerror(result), __FILE__, __LINE__); \
+            exit(EXIT_FAILURE); \
+        } \
+    } while (false)
 
 struct process {
     pid_t pid;
@@ -17,108 +29,87 @@ struct process {
     int stdout;
 };
 
-static bool process_spawn(struct process *p, char *const *argv);
+static struct process process_spawn(char *const *argv);
 static void *process_handler(void *p);
 
 static void *counter_thread(void *data);
-static unsigned int current_count(void);
 static ssize_t write_count(int pipe);
 
 int main(int argc, char **argv) {
-    struct process pico8;
     char *const pico8_args[] = {"pico8", "cart/controller.p8", NULL};
-    if (process_spawn(&pico8, pico8_args)) {
-        fprintf(stderr, "failed to spawn PICO-8 process\n");
-        return EXIT_FAILURE;
-    }
+    struct process pico8 = process_spawn(pico8_args);
 
     pthread_t count;
-    if (pthread_create(&count, NULL, counter_thread, NULL)) {
-        fprintf(stderr, "failed to start counter\n");
-        return EXIT_FAILURE;
-    }
+    TRY(pthread_create(&count, NULL, counter_thread, NULL));
 
     pthread_t handler;
-    if (pthread_create(&handler, NULL, process_handler, (void *)&pico8)) {
-        fprintf(stderr, "failed to start process handler\n");
-        return EXIT_FAILURE;
-    }
+    TRY(pthread_create(&handler, NULL, process_handler, (void *)&pico8));
 
-    waitpid(pico8.pid, NULL, 0);
+    if (waitpid(pico8.pid, NULL, 0) != pico8.pid) {
+        TRY(errno);
+    }
 
     return EXIT_SUCCESS;
 }
 
 extern char **environ;
 
-bool process_spawn(struct process *p, char *const *argv) {
+struct process process_spawn(char *const *argv) {
     const int R = 0;
     const int W = 1;
 
-    bool success = false;
-
     int stdin[2], stdout[2];
-    if (pipe(stdin) || pipe(stdout)) {
-        fprintf(stderr, "unable to open pipe\n");
-        goto err_pipes;
-    }
+    TRY(pipe(stdin));
+    TRY(pipe(stdout));
 
     posix_spawn_file_actions_t file_actions;
-    if (
-        posix_spawn_file_actions_init(&file_actions) ||
-        posix_spawn_file_actions_adddup2(&file_actions, stdin[R], STDIN_FILENO) ||
-        posix_spawn_file_actions_adddup2(&file_actions, stdout[W], STDOUT_FILENO)
-    ) {
-        fprintf(stderr, "error configuring file actions\n");
-        goto err_file_actions;
+    TRY(posix_spawn_file_actions_init(&file_actions));
+    TRY(posix_spawn_file_actions_adddup2(&file_actions, stdin[R], STDIN_FILENO));
+    TRY(posix_spawn_file_actions_adddup2(&file_actions, stdout[W], STDOUT_FILENO));
+    for (int i = 0; i < 2; ++i) {
+        TRY(posix_spawn_file_actions_addclose(&file_actions, stdin[i]));
+        TRY(posix_spawn_file_actions_addclose(&file_actions, stdout[i]));
     }
 
     sigset_t mask;
+    TRY(sigemptyset(&mask));
+
+    sigset_t def;
+    TRY(sigemptyset(&def));
+    TRY(sigaddset(&def, SIGPIPE));
+
     posix_spawnattr_t attr;
-    if (
-        sigemptyset(&mask) ||
-        posix_spawnattr_init(&attr) ||
-        posix_spawnattr_setsigmask(&attr, &mask) ||
-        sigaddset(&mask, SIGPIPE) ||
-        posix_spawnattr_setsigdefault(&attr, &mask) ||
-        posix_spawnattr_setflags(&attr, POSIX_SPAWN_SETSIGDEF | POSIX_SPAWN_SETSIGMASK)
-    ) {
-        fprintf(stderr, "error configuring spawn attributes\n");
-        goto err_attr;
-    }
+    TRY(posix_spawnattr_init(&attr));
+    TRY(posix_spawnattr_setsigmask(&attr, &mask));
+    TRY(posix_spawnattr_setsigdefault(&attr, &def));
+    TRY(posix_spawnattr_setflags(&attr, POSIX_SPAWN_SETSIGDEF | POSIX_SPAWN_SETSIGMASK));
 
-    success = posix_spawnp(&p->pid, argv[0], &file_actions, &attr, argv, environ) == 0;
+    pid_t pid;
+    TRY(posix_spawnp(&pid, argv[0], &file_actions, &attr, argv, environ));
 
+    close(stdin[R]);
+    close(stdout[W]);
+    posix_spawn_file_actions_destroy(&file_actions);
     posix_spawnattr_destroy(&attr);
 
-err_attr:
-    posix_spawn_file_actions_destroy(&file_actions);
-
-err_file_actions:
-    if (success) {
-        p->stdin = stdin[W];
-        p->stdout = stdout[R];
-    } else {
-        for (int i = 0; i < 2; i++) {
-            close(stdin[i]);
-            close(stdout[i]);
-        }
-    }
-
-err_pipes:
-    return !success;
+    return (struct process){
+        .pid = pid,
+        .stdin = stdin[W],
+        .stdout = stdout[R],
+    };
 }
 
 void *process_handler(void *data) {
-    struct process *p = (struct process *)data;
+    struct process p = *(struct process *)data;
     char buffer[1024];
 
-    write_count(p->stdin);
+    write_count(p.stdin);
     while (
-        read(p->stdout, buffer, sizeof(buffer)) > 0 &&
-        write_count(p->stdin) > 0
+        read(p.stdout, buffer, sizeof(buffer)) > 0 &&
+        write_count(p.stdin) > 0
     ) {}
 
+    TRY(errno);
     return NULL;
 }
 
@@ -138,24 +129,24 @@ void *counter_thread(void *data) {
     return NULL;
 }
 
-unsigned int current_count(void) {
-    return atomic_load(&counter);
-}
-
 ssize_t write_count(int pipe) {
-    uint32_t count = (uint32_t)current_count();
-
-    char *buffer = (char *)&count;
-    ssize_t rem = sizeof(count);
-    while (rem > 0) {
-        ssize_t n = write(pipe, buffer, rem);
-        buffer += n;
-        rem -= n;
-    }
+    union {
+        uint32_t count;
+        char bytes[5];
+    } buffer;
+    buffer.count = (uint32_t)atomic_load(&counter);
 
     // Make sure to write a newline, since it seems like PICO-8 will buffer
     // stdin reads until it gets one.
-    write(pipe, "\n", 1);
+    buffer.bytes[4] = '\n';
 
-    return sizeof(unsigned int) + 1;
+    char *ptr = buffer.bytes;
+    size_t rem = sizeof(buffer.bytes);
+    while (rem > 0) {
+        ssize_t n = write(pipe, ptr, rem);
+        ptr += n;
+        rem -= n;
+    }
+
+    return sizeof(buffer.bytes);
 }
