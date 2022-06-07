4a928c4dd9dc9139ee7d24d9bde113004d2090c5
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2022-06-07 22:26:44 +0200

    Undo refactor - not happy about it

--- Makefile
+++ Makefile
@@ -1,15 +1,3 @@
-CFLAGS := -std=c11 -Wall
-
-SRC := $(wildcard src/*.c)
-INC := $(wildcard src/*.h)
-OBJ := $(patsubst src/%,target/%,$(SRC:.c=.o))
-
-.PHONY: all
-all: target/pico8-controller
-
-target/pico8-controller: $(OBJ)
-	$(CC) -std=c11 -Wall -o $@ $^
-
-target/%.o: src/%.c $(INC)
+target/pico8-controller: src/main.c
 	mkdir -p target
-	$(CC) -c $(CFLAGS) $< -o $@
+	$(CC) -std=c11 $< -o $@ -Wall
--- src/main.c
+++ src/main.c
@@ -13,9 +13,23 @@
 
 #include <pthread.h>
 
-#include "pspawn.h"
-#include "try.h"
+#define TRY(x) \
+    do { \
+        int result; \
+        if ((result = (x)) != 0) { \
+            result = result == -1 ? errno : result; \
+            fprintf(stderr, "ERROR: %s (%s L%d)\n", strerror(result), __FILE__, __LINE__); \
+            exit(EXIT_FAILURE); \
+        } \
+    } while (false)
 
+struct process {
+    pid_t pid;
+    int stdin;
+    int stdout;
+};
+
+static struct process process_spawn(char *const *argv);
 static void *process_handler(void *p);
 
 static void *counter_thread(void *data);
@@ -23,7 +37,7 @@ static ssize_t write_count(int pipe);
 
 int main(int argc, char **argv) {
     char *const pico8_args[] = {"pico8", "cart/controller.p8", NULL};
-    struct process pico8 = pspawn(pico8_args);
+    struct process pico8 = process_spawn(pico8_args);
 
     pthread_t count;
     TRY(pthread_create(&count, NULL, counter_thread, NULL));
@@ -38,14 +52,61 @@ int main(int argc, char **argv) {
     return EXIT_SUCCESS;
 }
 
+extern char **environ;
+
+struct process process_spawn(char *const *argv) {
+    const int R = 0;
+    const int W = 1;
+
+    int stdin[2], stdout[2];
+    TRY(pipe(stdin));
+    TRY(pipe(stdout));
+
+    posix_spawn_file_actions_t file_actions;
+    TRY(posix_spawn_file_actions_init(&file_actions));
+    TRY(posix_spawn_file_actions_adddup2(&file_actions, stdin[R], STDIN_FILENO));
+    TRY(posix_spawn_file_actions_adddup2(&file_actions, stdout[W], STDOUT_FILENO));
+    for (int i = 0; i < 2; ++i) {
+        TRY(posix_spawn_file_actions_addclose(&file_actions, stdin[i]));
+        TRY(posix_spawn_file_actions_addclose(&file_actions, stdout[i]));
+    }
+
+    sigset_t mask;
+    TRY(sigemptyset(&mask));
+
+    sigset_t def;
+    TRY(sigemptyset(&def));
+    TRY(sigaddset(&def, SIGPIPE));
+
+    posix_spawnattr_t attr;
+    TRY(posix_spawnattr_init(&attr));
+    TRY(posix_spawnattr_setsigmask(&attr, &mask));
+    TRY(posix_spawnattr_setsigdefault(&attr, &def));
+    TRY(posix_spawnattr_setflags(&attr, POSIX_SPAWN_SETSIGDEF | POSIX_SPAWN_SETSIGMASK));
+
+    pid_t pid;
+    TRY(posix_spawnp(&pid, argv[0], &file_actions, &attr, argv, environ));
+
+    close(stdin[R]);
+    close(stdout[W]);
+    posix_spawn_file_actions_destroy(&file_actions);
+    posix_spawnattr_destroy(&attr);
+
+    return (struct process){
+        .pid = pid,
+        .stdin = stdin[W],
+        .stdout = stdout[R],
+    };
+}
+
 void *process_handler(void *data) {
     struct process p = *(struct process *)data;
     char buffer[1024];
 
-    write_count(p.in);
+    write_count(p.stdin);
     while (
-        read(p.out, buffer, sizeof(buffer)) > 0 &&
-        write_count(p.in) > 0
+        read(p.stdout, buffer, sizeof(buffer)) > 0 &&
+        write_count(p.stdin) > 0
     ) {}
 
     TRY(errno);
