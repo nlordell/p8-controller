60c7946a060ab5f56bc3e3d8194713f80b17dc33
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2022-06-20 23:32:20 +0200

    Browser support

--- Makefile
+++ Makefile
@@ -1,3 +1,21 @@
+PICO8 ?= pico8
+
+.PHONY: all
+all: target/p8-controller target/demo.html
+
 target/p8-controller: src/controller.c
 	mkdir -p target
 	$(CC) -std=c11 $< -o $@ -Wall `sdl2-config --cflags --libs`
+
+target/p8-controller.js: src/controller.js
+	mkdir -p target
+	cp $< $@
+
+target/demo.html: cart/demo.p8 target/p8-controller.js
+	mkdir -p target
+	$(PICO8) $< -export $@
+	sed 's|</body>|<script src="p8-controller.js"></script></body>|' $@ > target/sed.html
+	mv target/sed.html $@
+
+clean:
+	rm -rf target
--- src/controller.js
+++ src/controller.js
@@ -1,1 +1,47 @@
+pico8_gpio[0] = 0x9a;
 
+function p8_controller() {
+  const gamepads = [...navigator.getGamepads() ?? []].slice(0, 4);
+
+  let i = 1;
+  function putd(value) {
+    const int = Math.max(Math.min(~~(value * 0x8000), 0x7fff), -0x8000);
+    putc(int);
+    putc(int >> 8);
+  }
+  function putb(value) {
+    if (value) {
+      putc(0xff);
+    } else {
+      putc(0);
+    }
+  }
+  function putz(n) {
+    for (let i = 0; i < n; i++) {
+      putc(0);
+    }
+  }
+  function putc(value) {
+    pico8_gpio[i++] = value & 0xff;
+  }
+
+  putc(gamepads.length);
+  for (const gamepad of gamepads) {
+    if (gamepad === null || gamepad.mapping !== "standard") {
+      putz(30);
+    }
+
+    for (let i = 0; i < 4; i++) {
+      putd(gamepad.axes[i]);
+    }
+    putd(gamepad.buttons[6].value);
+    putd(gamepad.buttons[7].value);
+    for (let i = 0; i < 18; i++) {
+      const button = gamepad.buttons[i];
+      putb(button && button.pressed);
+    }
+  }
+
+  requestAnimationFrame(p8_controller);
+}
+requestAnimationFrame(p8_controller);
